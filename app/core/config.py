35c70efc6d7451d2b57b0e0dc9169ceae295037b
Kanysh <kanyshomirzak@gmail.com>
2026-06-15 15:36:15 +0500

    Rename main.py to .example.env and add environment variable settings; create config and database modules for async database connection

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    @property
+    def database_url(self) -> str:
+        return(
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    model_config = {"env_file": ".env"}
+
+settings = Settings()
