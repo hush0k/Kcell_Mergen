1472118dff4a0591fa7d52f2dd675a5d960d4289
Kanysh <kanyshomirzak@gmail.com>
2026-06-15 17:22:26 +0500

    Add user model and roles enumeration; configure Alembic for user table migration

---
+++ app/user/enums.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class UserRoles(enum.StrEnum):
+    ADMIN = "admin"
+    USER = "user"
--- app/user/model.py
+++ app/user/model.py
@@ -1,1 +1,21 @@
-# class User()
+from sqlalchemy import Enum, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+from app.user.enums import UserRoles
+
+
+class User(Base):
+    """ Модель пользователя системы """
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(256), nullable=False)
+    role: Mapped[UserRoles] = mapped_column(
+        Enum(UserRoles), default=UserRoles.USER, nullable=False # UserRole может быть ADMIN либо USER
+    )
+
+
+
+
