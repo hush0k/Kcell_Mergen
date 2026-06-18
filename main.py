8a9b4256a9a494d094d12027fa16a1cea40ff4a7
Kanysh <kanyshomirzak@gmail.com>
2026-06-18 09:15:09 +0500

    Add task router to main application for task management functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from app.auth.router import router as auth_router
 from app.control.router import router as control_router
+from app.task.router import router as task_router
 from app.db.database import create_schema
 from app.user.router import router as user_router
 
@@ -20,6 +21,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(control_router)
+app.include_router(task_router)
 
 
 @app.get("/")
