2786d18b1e97ed7905a23f5a3bfb5699644ecebb
Kanysh <kanyshomirzak@gmail.com>
2026-06-22 16:56:08 +0500

    Add notification model import to environment configuration

--- migrations/env.py
+++ migrations/env.py
@@ -12,6 +12,7 @@ import app.task.model  # noqa: F401
 import app.task.non_working_day  # noqa: F401
 import app.user.model  # noqa: F401
 import app.vacation_schedule.model  # noqa: F401
+import app.notification.model  # noqa: F401
 from app.core.config import settings
 from app.db.database import Base
 
