ec0607f7bb36188a2a1dbf31fd9a2c24ccf696af
Kanysh <kanyshomirzak@gmail.com>
2026-06-25 12:10:51 +0500

    Implement PrivateRoute component for protected routes; update App routing to include navigation and redirect logic

--- new_frontend/src/app/App.tsx
+++ new_frontend/src/app/App.tsx
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "@/pages/LoginPage";
 import { HomePage } from "@/pages/HomePage"
+import { PrivateRoute } from '@/components/PrivateRoute'
+import { Navigate } from 'react-router-dom'
 
 export function App() {
     return (
         <main className="mg-app-shell min-h-screen bg-mg-bg text-mg-text">
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/home" element={<HomePage />} />
-                {/* сюда добавляй остальные страницы */}
+                <Route path="/home" element={
+                    <PrivateRoute>
+                        <HomePage />
+                    </PrivateRoute>
+                } />
+                <Route path="/" element={<Navigate to="/home" replace />} />
+
             </Routes>
         </main>
     );
